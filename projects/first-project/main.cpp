d8ba2799dd7edbade4d2b64e5b6f7998f0c01cdb
Egyra2007 <135865282+Egyra2007@users.noreply.github.com>
2023-06-08 10:59:01 -0700

    Update main.cpp

--- projects/first-project/main.cpp
+++ projects/first-project/main.cpp
@@ -1,81 +1,65 @@
-#include <iostream>
-#include <vector>
-#include <limits>
-
 class Mountain {
-private:
-    std::string name;
-    std::string country;
-    double elevation;
-
-public:
-    Mountain(const std::string& name, const std::string& country, double elevation)
-        : name(name), country(country), elevation(elevation) {}
-
-    std::string getName() const {
-        return name;
-    }
-
-    std::string getCountry() const {
-        return country;
-    }
-
-    double getElevation() const {
-        return elevation;
-    }
-
-    void setName(const std::string& name) {
-        this->name = name;
-    }
-
-    void setCountry(const std::string& country) {
-        this->country = country;
-    }
-
-    void setElevation(double elevation) {
-        this->elevation = elevation;
-    }
-
-    double toMeters() const {
-        return elevation / 3.2808;
-    }
-};
-
-Mountain minElevation(const std::vector<Mountain>& mountains) {
-    Mountain minMountain = mountains[0];
-    for (const Mountain& mountain : mountains) {
-        if (mountain.getElevation() < minMountain.getElevation()) {
-            minMountain = mountain;
-        }
-    }
-    return minMountain;
+   private String name;
+   private String country;
+   private int elevation;
+   public Mountain(String name, String country, int elevation) {
+       this.name = name;
+       this.country = country;
+       this.elevation = elevation;
+   }
+   public String getName() {
+       return name;
+   }
+   public String getCountry() {
+       return country;
+   }
+   public int getElevation() {
+       return elevation;
+   }
+   public void setName(String name) {
+       this.name = name;
+   }
+   public void setCountry(String country) {
+       this.country = country;
+   }
+   public void setElevation(int elevation) {
+       this.elevation = elevation;
+   }
+   public double toMeters() {
+       return elevation / 3.2808;
+   }
 }
-
-int main() {
-    std::vector<Mountain> mountains = {
-        Mountain("Chimborazo", "Ecuador", 20549),
-        Mountain("Matterhorn", "Switzerland", 14692),
-        Mountain("Olympus", "Greece (Macedonia)", 9573),
-        Mountain("Everest", "Nepal", 29029),
-        Mountain("Mount Marcy - Adirondacks", "United States", 5344),
-        Mountain("Mount Mitchell - Blue Ridge", "United States", 6684),
-        Mountain("Zugspitze", "Switzerland", 9719)
-    };
-
-    // Print mountain details and convert elevation to meters
-    for (const Mountain& mountain : mountains) {
-        std::cout << "Mountain: " << mountain.getName() << std::endl;
-        std::cout << "Country: " << mountain.getCountry() << std::endl;
-        std::cout << "Elevation (ft): " << mountain.getElevation() << std::endl;
-        std::cout << "Elevation (m): " << mountain.toMeters() << std::endl;
-        std::cout << std::endl;
-    }
-
-    // Find minimum elevation
-    Mountain minElevationMountain = minElevation(mountains);
-    std::cout << "Shortest Mountain: " << minElevationMountain.getName() << std::endl;
-    std::cout << "Elevation (ft): " << minElevationMountain.getElevation() << std::endl;
-    std::cout << "Elevation (m): " << minElevationMountain.toMeters() << std::endl;
-
-    return 0;
+public class Main {
+   public static Mountain minElevation(Mountain[] mountains) {
+       int minElevation = Integer.MAX_VALUE;
+       Mountain shortestMountain = null;
+       for (Mountain mountain : mountains) {
+           if (mountain.getElevation() < minElevation) {
+               minElevation = mountain.getElevation();
+               shortestMountain = mountain;
+           }
+       }
+       return shortestMountain;
+   }
+   public static void main(String[] args) {
+       Mountain[] mountains = {
+               new Mountain("Chimborazo", "Ecuador", 20549),
+               new Mountain("Matterhorn", "Switzerland", 14692),
+               new Mountain("Olympus", "Greece (Macedonia)", 9573),
+               new Mountain("Everest", "Nepal", 29029),
+               new Mountain("Mount Marcy - Adirondacks", "United States", 5344),
+               new Mountain("Mount Mitchell - Blue Ridge", "United States", 6684),
+               new Mountain("Zugspitze", "Switzerland", 9719)
+       };
+       Mountain shortestMountain = minElevation(mountains);
+       for (Mountain mountain : mountains) {
+           System.out.println("Mountain: " + mountain.getName() + " (" + mountain.getCountry() + ")");
+           System.out.println("Elevation (feet): " + mountain.getElevation());
+           System.out.println("Elevation (meters): " + mountain.toMeters());
+           System.out.println();
+       }
+       System.out.println("Shortest Mountain: " + shortestMountain.getName() + " (" + shortestMountain.getCountry() + ")");
+       System.out.println("Elevation (feet): " + shortestMountain.getElevation());
+       System.out.println("Elevation (meters): " + shortestMountain.toMeters());
+   }
 }
